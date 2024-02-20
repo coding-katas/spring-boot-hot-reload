db855a96bce3967896ef4a6a24e5ecb2fe15eb1a
anis Tajouri <anis.tajouri@gmail.com>
2024-02-20 20:11:41 +0000

    update config

--- bootstrap/cleanup.sh
+++ bootstrap/cleanup.sh
@@ -21,3 +21,16 @@ rm -rf $TF_DIR/.terraform
 rm -rf $TF_DIR/main.tf
 rm -rf $TF_DIR/terraform.tfstat*
 rm -rf $TF_DIR/terraform.tfplan
+
+gcloud compute routers delete app-router --region=us-central1 --quiet
+gcloud compute networks subnets delete app-subnetwork --region=us-central1 --quiet
+gcloud compute networks delete app-network --quiet
+#gcloud compute firewall-rules delete allow-ssh
+#gcloud compute firewall-rules delete allow-health-check
+#gcloud compute backend-services delete helloworld-backend-service
+#gcloud compute url-maps delete helloworld-url-map
+#gcloud compute target-http-proxies delete helloworld-target-http-proxy
+#gcloud compute forwarding-rules delete helloworld-forwarding-rule
+gcloud iam service-accounts delete tf-sa-staging@${PROJECT_ID}.iam.gserviceaccount.com --quiet
+gcloud iam service-accounts delete cd-tut-deploy-sa@${PROJECT_ID}.iam.gserviceaccount.com --quiet
+gcloud iam service-accounts delete tf-sa-test@${PROJECT_ID}.iam.gserviceaccount.com --quiet
